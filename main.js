572d364f97462b6b4fa68a4a52e0f41d7ab05031
Magnus Kühn <magnus.khn@gmail.com>
2017-02-11 22:32:54 +0100

    React to scrolling

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 "use strict";
 
-const iterations = 200;
-const reds = [], greens = [];
-
 // calculate colors as micro task
+const reds = [], greens = [];
 (function () {
     let offsetRed = 10, offsetGreen = 10;
     for (let i = 0; i <= 200; i++) {
@@ -37,21 +35,38 @@ const reds = [], greens = [];
 
 
     // TODO: construct matching state
-    let offsetX = -width / 2 - 200;
-    let offsetY = -height / 2;
+    let offsetX = width / 2 + 200;
+    let offsetY = height / 2;
     let zoom = 350;
+    let iterations = 10 * Math.sqrt(zoom);
 
 
     // react to mouse drags
     let downEvent = null;
     canvas.addEventListener('mousedown', event => downEvent = event);
     canvas.addEventListener('mouseup', event => {
-        offsetX -= event.clientX - downEvent.clientX;
-        offsetY -= event.clientY - downEvent.clientY;
+        offsetX += event.clientX - downEvent.clientX;
+        offsetY += event.clientY - downEvent.clientY;
         downEvent = null;
 
         renderedX = 0;
     });
+
+    // react to scrolling
+    const scrollHandler = event => {
+        // there shall be a fixed point under the cursor
+        const delta = event.wheelDelta ? event.wheelDelta * 10 : -event.detail * 400;
+        const ratio = (zoom + delta) / zoom;
+        offsetX = event.clientX - ratio * (event.clientX - offsetX);
+        offsetY = event.clientY - ratio * (event.clientY - offsetY);
+
+        // redraw everything
+        zoom += delta;
+        iterations = 10 * Math.sqrt(zoom);
+        renderedX = 0;
+    };
+    canvas.addEventListener('DOMMouseScroll', scrollHandler);
+    canvas.addEventListener('mousewheel', scrollHandler);
 
 
     // render continuously
@@ -63,7 +78,7 @@ const reds = [], greens = [];
             // render progressively in columns
             for (; x <= renderedX; x++) {
                 for (let y = 0; y <= height; y++) {
-                    const n = stepsToDivergence((x + offsetX) / zoom, (y + offsetY) / zoom, iterations);
+                    const n = stepsToDivergence((x - offsetX) / zoom, (y - offsetY) / zoom, iterations);
                     context.fillStyle = colorFromSteps(n, iterations);
                     context.fillRect(x, y, 1, 1);
                 }
