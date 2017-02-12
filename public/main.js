9a838336db9a651ee8dbe5b05b020377a2f376a5
Magnus Kühn <magnus.khn@gmail.com>
2017-02-12 21:48:29 +0100

    Render first under cursor

--- public/main.js
+++ public/main.js
@@ -1,53 +1,60 @@
 "use strict";
 
-const register = 'requestIdleCallback' in window ? requestIdleCallback : requestAnimationFrame;
+const RENDER_CHUNK = 100;
+const REQUEST_IDLE = 'requestIdleCallback' in window ? requestIdleCallback : f => {
+    };
+
 
 // render fractal
 (function () {
-    let width, height, offsetX, offsetY, renderedX;
+    let width, height, offsetX, offsetY, renderCurrentX, renderAgain;
     let zoom = 350;
     let iterations = 10 * Math.sqrt(zoom);
 
     // setup continuous rendering
-    let isTiming = false;
     const canvas = document.querySelector('canvas');
     const context = canvas.getContext('2d');
     const render = () => {
-        if (renderedX < width) {
-            if (!isTiming) {
-                console.time('rendering');
-                isTiming = true;
-            }
-
+        if (renderCurrentX < width) {
             // Chrome 56 cannot optimize compound let assignments
-            let x = renderedX;
-            renderedX = renderedX + 100;
-            // renderedX += 100;
+            let x = renderCurrentX;
+            renderCurrentX = renderCurrentX + RENDER_CHUNK;
+            // renderCurrentX += RENDER_CHUNK;
 
             // render progressively in columns
-            for (; x <= renderedX; x++) {
+            for (; x <= renderCurrentX; x++) {
                 for (let y = 0; y <= height; y++) {
                     context.fillStyle = colorAtPoint((x - offsetX) / zoom, (y - offsetY) / zoom, iterations);
                     context.fillRect(x, y, 1, 1);
                 }
             }
-
-            // mark current rendering position
-            context.fillStyle = 'white';
-            context.fillRect(x, 0, 1, height);
-        } else if (isTiming) {
-            console.timeEnd('rendering');
-            isTiming = false;
+        } else if (renderAgain) {
+            console.log('rendering again');
+            renderCurrentX = 0;
+            renderAgain = false;
         }
-
-        register(render);
     };
+
+    // callback renders
+    const renderAnimation = () => {
+        render();
+        requestAnimationFrame(renderAnimation);
+    };
+    const renderIdle = () => {
+        render();
+
+        if (renderCurrentX < width) {
+            requestIdleCallback(renderIdle);
+        }
+    };
+
 
     // react to resizing
     const resizeHandler = () => {
         canvas.width = width = document.body.clientWidth;
         canvas.height = height = document.body.clientHeight;
-        renderedX = 0;
+        renderCurrentX = 0;
+        REQUEST_IDLE(renderIdle);
     };
     window.addEventListener('resize', resizeHandler);
 
@@ -60,7 +67,9 @@ const register = 'requestIdleCallback' in window ? requestIdleCallback : request
         offsetY += event.clientY - downEvent.clientY;
         downEvent = null;
 
-        renderedX = 0;
+        renderCurrentX = event.clientX - RENDER_CHUNK / 2;
+        renderAgain = true;
+        REQUEST_IDLE(renderIdle);
     });
 
 
@@ -93,7 +102,9 @@ const register = 'requestIdleCallback' in window ? requestIdleCallback : request
 
         // apparently 64bit doubles can't be more precise than 800 iterations
         iterations = Math.min(100 + 0.8 * Math.sqrt(zoom), 800);
-        renderedX = 0;
+        renderCurrentX = event.clientX - RENDER_CHUNK / 2;
+        renderAgain = true;
+        REQUEST_IDLE(renderIdle);
     };
     canvas.addEventListener('DOMMouseScroll', scrollHandler);
     canvas.addEventListener('mousewheel', scrollHandler);
@@ -103,7 +114,8 @@ const register = 'requestIdleCallback' in window ? requestIdleCallback : request
     resizeHandler();
     offsetX = width / 2 + 200;
     offsetY = height / 2;
-    register(render);
+    requestAnimationFrame(renderAnimation);
+    REQUEST_IDLE(renderIdle);
 })();
 
 
